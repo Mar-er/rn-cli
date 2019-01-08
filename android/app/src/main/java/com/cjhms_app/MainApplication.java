3f5c27cf00ceddba49366f721ed6a710f34e3815
Mar-er <243958407@qq.com>
2019-01-08 17:45:21 +0800

    add：添加渐变配置

--- android/app/src/main/java/com/cjhms_app/MainApplication.java
+++ android/app/src/main/java/com/cjhms_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.cjhms_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage(),
--- src/views/Account/SignIn/SignIn.js
+++ src/views/Account/SignIn/SignIn.js
@@ -6,12 +6,23 @@ import Svg, {
   Circle, Rect,
 } from 'react-native-svg';
 import SvgUri from 'react-native-svg-uri';
+import LinearGradient from 'react-native-linear-gradient';
 import { VictoryBar } from 'victory-native';
 import Icon from '../../../components/Icon';
 import CSvgUri from '../../../components/Svg';
 import LiquidFill from '../../../components/LiquidFill';
 
-const { width, height } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  linearGradient: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 50,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+});
 export default ({ navigation }) => (
   <View>
     <Text>SignIn</Text>
@@ -20,43 +31,41 @@ export default ({ navigation }) => (
       onPress={() => { navigation.navigate('App'); }}
     />
     <Icon name="dingzheng" size={60} />
-    <View
-      style={[
-        StyleSheet.absoluteFill,
-        { alignItems: 'center', justifyContent: 'center' },
-      ]}
-    >
-      <Svg height={height * 0.5} width={width * 0.5} viewBox="0 0 100 100">
-        <Circle
-          cx="50"
-          cy="50"
-          r="45"
-          stroke="blue"
-          strokeWidth="2.5"
-          fill="green"
-        />
-        <Rect
-          x="15"
-          y="15"
-          width="70"
-          height="70"
-          stroke="red"
-          strokeWidth="2"
-          fill="yellow"
-        />
-      </Svg>
-      <SvgUri
-        width="200"
-        height="200"
-        source={{ uri: 'http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg' }}
+    <Svg height={300} width={300} viewBox="0 0 100 100">
+      <Circle
+        cx="50"
+        cy="50"
+        r="45"
+        stroke="blue"
+        strokeWidth="2.5"
+        fill="green"
       />
-      <CSvgUri
-        width="200"
-        height="200"
-        source="no-data"
+      <Rect
+        x="15"
+        y="15"
+        width="70"
+        height="70"
+        stroke="red"
+        strokeWidth="2"
+        fill="yellow"
       />
-      <VictoryBar />
-      <LiquidFill />
-    </View>
+    </Svg>
+    <SvgUri
+      width="200"
+      height="200"
+      source={{ uri: 'http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg' }}
+    />
+    <CSvgUri
+      width="200"
+      height="200"
+      source="no-data"
+    />
+    <VictoryBar />
+    <LiquidFill />
+    <LinearGradient colors={['#FFD801', '#FF8040', '#F75D59']} style={styles.linearGradient}>
+      <Text style={{ color: '#fff' }}>
+          Sign in with Facebook
+      </Text>
+    </LinearGradient>
   </View>
 );
